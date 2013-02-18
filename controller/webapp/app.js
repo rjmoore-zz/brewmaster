6fc3bd8335f6fa2e4bb5d3f5d5d6cc6ba3e8db85
Richard Moore <richardmoore64@hotmail.com>
2013-02-18 23:11:53 +0000

    Replaced old chart pages with all-time chart

--- controller/webapp/app.js
+++ controller/webapp/app.js
@@ -2,63 +2,24 @@ var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3');
 
-
 // ----------------------------------------------------------
 
 app.use(express.logger());
-
 app.use(express.static('../static'));
 
 // ----------------------------------------------------------
+
 app.get('/', function(req, res) {
-  res.send('Hello World');
-});
-
-app.get('/basic-temperature-chart-data', function(req, res) {
   
     var rows = [];
             
     var temperatureDB = new sqlite3.Database("/home/pi/dev/db/temperature.db");
     temperatureDB.serialize(function() {
 
-        temperatureDB.all(
-            "SELECT reading_time,temperature " +
-            "FROM temperatures " +
-            "WHERE temperature BETWEEN 0 AND 40 AND " +
-            "      reading_time > date('now','-1 day') " +
-            "ORDER BY reading_time", 
-        function(err, rowsData) {
-            var diff = 0;
-            var date;
-            if (rowsData !== undefined) {
-            for (n = 1;n < rowsData.length;n++) {
-                // display if temperature differential < 0.5 degree 
-                if (n > 1) {
-                    diff = Math.abs(rowsData[n].temperature - rowsData[n-1].temperature);
-                }
-                if (diff < 0.5) {
-                    date = new Date(rowsData[n].reading_time);
-                    rows.push({Date : date.getTime(), Value : rowsData[n].temperature});
-                }
-            }
-            }
-            res.send(JSON.stringify(rows));
-
-        });
-        
-    });
-    temperatureDB.close();
-
-});
-
-
-app.get('/all-time-temperature-chart-data', function(req, res) {
-  
-    var rows = [];
-            
-    var temperatureDB = new sqlite3.Database("/home/pi/dev/db/temperature.db");
-    temperatureDB.serialize(function() {
-
+/* example for date range selection:
+    "WHERE temperature BETWEEN 0 AND 40 AND " +
+    "      reading_time > date('now','-1 day') " +
+ */
         temperatureDB.all(
             "SELECT reading_time,temperature " +
             "FROM temperatures " +
@@ -68,16 +29,16 @@ app.get('/all-time-temperature-chart-data', function(req, res) {
             var diff = 0;
             var date;
             if (rowsData !== undefined) {
-            for (n = 1;n < rowsData.length;n++) {
-                // display if temperature differential < 0.5 degree 
-                if (n > 1) {
-                    diff = Math.abs(rowsData[n].temperature - rowsData[n-1].temperature);
+                for (n = 1;n < rowsData.length;n++) {
+                    // display if temperature differential < 0.5 degree 
+                    if (n > 1) {
+                        diff = Math.abs(rowsData[n].temperature - rowsData[n-1].temperature);
+                    }
+                    if (diff < 0.5) {
+                        date = new Date(rowsData[n].reading_time);
+                        rows.push({Date : date.getTime(), Value : rowsData[n].temperature});
+                    }
                 }
-                if (diff < 0.5) {
-                    date = new Date(rowsData[n].reading_time);
-                    rows.push({Date : date.getTime(), Value : rowsData[n].temperature});
-                }
-            }
             }
             res.send(JSON.stringify(rows));
 
